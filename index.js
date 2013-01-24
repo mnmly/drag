6c49fbbffb8ca61836b9684551c1076988921417
mnmly <i.am@mnmly.com>
2013-01-23 23:27:27 -0800

    Pass element on event

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ Drag.prototype.ontouchstart = function( e ) {
   this.docEvents.bind( 'touchend' );
   this.docEvents.bind( 'mouseup', 'ontouchend' );
 
-  this.emit( 'dragstart' );
+  this.emit( 'dragstart' , this.el );
 };
 
 /**
@@ -112,7 +112,7 @@ Drag.prototype.ontouchmove = function( e ) {
     this.el.style.left = x;
     this.el.style.top  = y;
   }
-  this.emit( 'drag' );
+  this.emit( 'drag', this.el );
 };
 
 /**
@@ -125,5 +125,5 @@ Drag.prototype.ontouchmove = function( e ) {
 
 Drag.prototype.ontouchend = function( e ) {
   this.docEvents.unbind();
-  this.emit( 'dragend' );
+  this.emit( 'dragend', this.el );
 };

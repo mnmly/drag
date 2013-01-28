9da4a67ba00e9267e4ba0181c461e3d8135b3726
mnmly <i.am@mnmly.com>
2013-01-27 17:36:05 -0800

    Add support for transalte3d

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * Module dependencies
  */
 
-var events  = require( 'events' ),
-    Emitter = require( 'emitter' );
+var events    = require( 'events' ),
+    CSSMatrix = require( './css-matrix' ),
+    Emitter   = require( 'emitter' );
 
 /**
  * Export `Drag`
@@ -12,8 +13,14 @@ var events  = require( 'events' ),
 module.exports = Drag;
 
 /**
+ * Firefox flag
+ */
+
+var isFF = navigator.userAgent.search('Firefox') > -1;
+
+/**
  * Tuen `el` into draggable element.
- * 
+ *
  * Emits:
  *
  *   - `dragstart` when drag starts
@@ -29,16 +36,16 @@ function Drag( el, smooth ){
   if ( !( this instanceof Drag ) ) return new Drag( el, smooth );
   if ( !el ) throw new TypeError( 'Drag() requires an element' );
   Emitter.call( this );
-  this.el = el;
+  this.el     = el;
   this.smooth = smooth;
   this.bind();
-};
+}
 
 /**
- * Mixin `Emitter`
+ * Inherit `Emitter`
  */
 
-Emitter( Drag.prototype );
+Drag.prototype.__proto__ = Emitter.prototype;
 
 /**
  * Bind event handlers
@@ -63,7 +70,6 @@ Drag.prototype.unbind = function() {
   this.events.unbind();
 };
 
-
 /**
  * Handle touchstart
  * 
@@ -81,12 +87,15 @@ Drag.prototype.ontouchstart = function( e ) {
   this.originY = this.el.offsetTop;
   this.startX  = e.pageX;
   this.startY  = e.pageY;
-  
+  if( this.smooth ){
+    var translate   = this.getTranslate();
+    this.translateX = translate.x;
+    this.translateY = translate.y;
+  }
   this.docEvents.bind( 'touchmove' );
   this.docEvents.bind( 'mousemove', 'ontouchmove' );
   this.docEvents.bind( 'touchend' );
   this.docEvents.bind( 'mouseup', 'ontouchend' );
-
   this.emit( 'dragstart' , e );
 };
 
@@ -100,17 +109,18 @@ Drag.prototype.ontouchstart = function( e ) {
 
 Drag.prototype.ontouchmove = function( e ) {
   e.preventDefault();
-  var x = ( this.originX + ( e.pageX - this.startX )) + 'px',
-      y = ( this.originY + ( e.pageY - this.startY )) + 'px';
+  this.x = this.originX + ( e.pageX - this.startX ),
+  this.y = this.originY + ( e.pageY - this.startY );
   if( this.smooth ){
+    this.x -= this.originX - this.translateX;
+    this.y -= this.originY - this.translateY;
     this.el.style.webkitTransform = 
        this.el.style.mozTransform = 
         this.el.style.msTransform = 
-          this.el.style.transform = 'translate3d( ' + [ x, y ] + ', 0 )';
-
+          this.el.style.transform = 'translate3d( ' + this.x + 'px, ' + this.y + 'px , 0.0001px )'; // need to force `matrix3d`
   } else {
-    this.el.style.left = x;
-    this.el.style.top  = y;
+    this.el.style.left = this.x + 'px';
+    this.el.style.top  = this.y + 'px';
   }
   this.emit( 'drag', e );
 };
@@ -126,4 +136,30 @@ Drag.prototype.ontouchmove = function( e ) {
 Drag.prototype.ontouchend = function( e ) {
   this.docEvents.unbind();
   this.emit( 'dragend', e );
+};
+
+/**
+ * Returns translate value
+ *
+ * @return {Object} x and y coordinate of `translate3d`
+ * 
+ * @api private
+ */
+
+Drag.prototype.getTranslate = function( ) {
+  var x = 0, y = 0,
+      transform = getComputedStyle( this.el )[isFF ? 'transform' : 'webkitTransform'],
+      matrix, rawMatrix;
+  if( transform != 'none' ){
+    if( isFF ) {
+      rawMatrix = transform.replace(/[a-z\(\)(3d)]/g, '').split( ',' ).map( function(v){ return v * 1; } ); 
+      matrix = new CSSMatrix();
+      CSSMatrix.apply( matrix, rawMatrix );
+    } else {
+      matrix = new WebKitCSSMatrix( transform );
+    }
+    x = matrix.m41;
+    y = matrix.m42;
+  }
+  return { x: x, y: y };
 };
